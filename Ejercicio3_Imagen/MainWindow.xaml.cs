90ce14c53987a86930d5e6d40d5f58ef860068ac
susana <susana.casas@iesdoctorbalmis.com>
2020-10-26 14:46:59 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3_Imagen/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Ejercicio3_Imagen
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            opacidadAlta.IsChecked = true;
+            relleno.IsChecked = true;
+        }
+
+        private void OpacidadAlta_Checked(object sender, RoutedEventArgs e)
+        {
+            imagen.Opacity = 0.9;
+        }
+
+        private void OpacidadMedia_Checked(object sender, RoutedEventArgs e)
+        {
+            imagen.Opacity = 0.6;
+        }
+
+        private void OpacidadBaja_Checked(object sender, RoutedEventArgs e)
+        {
+            imagen.Opacity = 0.3;
+        }
+
+        private void Relleno_Checked(object sender, RoutedEventArgs e)
+        {
+            imagen.Stretch = Stretch.Fill;
+        }
+
+        private void Uniforme_Checked(object sender, RoutedEventArgs e)
+        {
+            imagen.Stretch = Stretch.Uniform;
+        }
+
+        private void RellenoUniforme_Checked(object sender, RoutedEventArgs e)
+        {
+            imagen.Stretch = Stretch.UniformToFill;
+        }
+
+        private void SinAjuste_Checked(object sender, RoutedEventArgs e)
+        {
+            imagen.Stretch = Stretch.None;
+        }
+    }
+}
